f6515879c025b548ec308b6d114d0560c9acf774
Matthew <Matthew@DESKTOP-RD7MSLN>
2019-02-14 13:43:46 +1000

    created react app alongside backend.

---
+++ frontend/src/components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        <a href="./">
+          <h1>Adnat</h1>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Header;
